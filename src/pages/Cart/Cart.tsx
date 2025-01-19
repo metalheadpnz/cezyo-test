64627f9951d4a9c515d67b28b4c7718ad37261b6
metalheadpnz <metalheadpnz@gmail.com>
2025-01-19 23:07:54 +0300

    add prettier

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
 
 export const Cart: React.FC = () => {
-    return (
-        <div>
-            <h2>Корзина</h2>
-        </div>
-    );
-};
+
+  return (
+    <div>
+      <h2>Корзина</h2>
+    </div>
+  )
+}
